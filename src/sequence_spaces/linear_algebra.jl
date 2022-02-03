21912981be6e6387e79ed6a52067f7ebd7c1ca4d
Olivier <olivier.henot@mail.mcgill.ca>
2022-02-03 16:05:33 -0500

    Add more linear algebra functions

--- src/sequence_spaces/linear_algebra.jl
+++ src/sequence_spaces/linear_algebra.jl
@@ -1,16 +1,26 @@
+# sparse
+
+SparseArrays.sparse(a::Sequence) = Sequence(space(a), sparse(coefficients(a)))
+SparseArrays.sparse(A::LinearOperator) = LinearOperator(domain(A), codomain(A), sparse(coefficients(A)))
+
 # condition number
 
 LinearAlgebra.cond(A::LinearOperator, p::Real=2) = cond(coefficients(A), p)
 
 # transpose, adjoint
 
-LinearAlgebra.transpose(A::LinearOperator) = transpose(coefficients(A))
+LinearAlgebra.transpose(a::Sequence) = LinearOperator(space(a), ParameterSpace(), transpose(coefficients(a)))
+LinearAlgebra.transpose(A::LinearOperator) = LinearOperator(codomain(A), domain(A), transpose(coefficients(A)))
 
-LinearAlgebra.adjoint(A::LinearOperator) = adjoint(coefficients(A))
+LinearAlgebra.adjoint(a::Sequence) = LinearOperator(space(a), ParameterSpace(), adjoint(coefficients(a)))
+LinearAlgebra.adjoint(A::LinearOperator) = LinearOperator(codomain(A), domain(A), adjoint(coefficients(A)))
 
 # eigen
 
-LinearAlgebra.eigvals(A::LinearOperator; kwargs...) = eigvals(coefficients(A); kwargs...)
+function LinearAlgebra.eigvals(A::LinearOperator; kwargs...)
+    domain(A) == codomain(A) || throw(DimensionMismatch)
+    return eigvals(coefficients(A); kwargs...)
+end
 
 function LinearAlgebra.eigvecs(A::LinearOperator; kwargs...)
     domain(A) == codomain(A) || throw(DimensionMismatch)
@@ -19,6 +29,7 @@ function LinearAlgebra.eigvecs(A::LinearOperator; kwargs...)
 end
 
 function LinearAlgebra.eigen(A::LinearOperator; kwargs...)
+    domain(A) == codomain(A) || throw(DimensionMismatch)
     Λ, Ξ = eigen(coefficients(A); kwargs...)
     return Λ, LinearOperator(ParameterSpace()^size(Ξ, 2), domain(A), Ξ)
 end

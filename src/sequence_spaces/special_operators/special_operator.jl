2fea6d60564ec0b2349a7b2211ef365750b80ad2
OlivierHnt <38465572+OlivierHnt@users.noreply.github.com>
2022-12-30 13:28:16 -0800

    Fix `+`/`-` with `SpecialOperator`

--- src/sequence_spaces/special_operators/special_operator.jl
+++ src/sequence_spaces/special_operators/special_operator.jl
@@ -41,21 +41,21 @@ mul!(c::Sequence, S::SpecialOperator, a::Sequence, α::Number, β::Number) =
 
 function Base.:+(A::LinearOperator, S::SpecialOperator)
     domain_A = domain(A)
-    new_codomain = image(add_bar, codomain(A), image(S, domain_A))
+    new_codomain = image(+, codomain(A), image(S, domain_A))
     return ladd!(A, project(S, domain_A, new_codomain, _coeftype(S, domain_A, eltype(A))))
 end
 function Base.:+(S::SpecialOperator, A::LinearOperator)
     domain_A = domain(A)
-    new_codomain = image(add_bar, image(S, domain_A), codomain(A))
+    new_codomain = image(+, image(S, domain_A), codomain(A))
     return radd!(project(S, domain_A, new_codomain, _coeftype(S, domain_A, eltype(A))), A)
 end
 function Base.:-(A::LinearOperator, S::SpecialOperator)
     domain_A = domain(A)
-    new_codomain = image(sub_bar, codomain(A), image(S, domain_A))
+    new_codomain = image(-, codomain(A), image(S, domain_A))
     return lsub!(A, project(S, domain_A, new_codomain, _coeftype(S, domain_A, eltype(A))))
 end
 function Base.:-(S::SpecialOperator, A::LinearOperator)
     domain_A = domain(A)
-    new_codomain = image(sub_bar, image(S, domain_A), codomain(A))
+    new_codomain = image(-, image(S, domain_A), codomain(A))
     return rsub!(project(S, domain_A, new_codomain, _coeftype(S, domain_A, eltype(A))), A)
 end

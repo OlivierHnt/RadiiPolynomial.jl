4d04cb550de22517dfcd68544ae4b33e0f2ce6b0
OlivierHnt <38465572+OlivierHnt@users.noreply.github.com>
2023-12-17 19:16:01 +0100

    Export `weight`

--- src/RadiiPolynomial.jl
+++ src/RadiiPolynomial.jl
@@ -42,7 +42,7 @@ import LinearAlgebra: norm, opnorm
 include("sequence_spaces/norm.jl")
     export Weight, IdentityWeight, GeometricWeight, geometricweight,
         AlgebraicWeight, algebraicweight, BesselWeight, rate,
-        BanachSpace, Ell1, ℓ¹, Ell2, ℓ², EllInf, ℓ∞, NormedCartesianSpace
+        BanachSpace, Ell1, ℓ¹, Ell2, ℓ², EllInf, ℓ∞, NormedCartesianSpace, weight
 
     export norm, opnorm
 

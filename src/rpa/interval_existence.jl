1900d3d577e6f81c115d8096c807ccb626bc7df4
OlivierHnt <38465572+OlivierHnt@users.noreply.github.com>
2024-01-21 15:26:20 +0100

    Add a verbose option to `interval_of_existence`

--- src/rpa/interval_existence.jl
+++ src/rpa/interval_existence.jl
@@ -3,69 +3,85 @@
 
 Return an interval of existence ``I \\subset [0, R]`` such that ``Y + (Z_1 - 1) r \\le 0`` and ``Z_1 < 1`` for all ``r \\in I``.
 """
-function interval_of_existence(Y_::Real, Z₁_::Real, R::Real)
-    Y, Z₁ = sup(Y_), sup(Z₁_)
-    NewType = float(promote_type(typeof(Y), typeof(Z₁), typeof(R)))
-    if !(Y ≥ 0 && isfinite(Y) && Z₁ ≥ 0 && isfinite(Z₁) && R ≥ 0)
-        return emptyinterval(NewType) # throw(DomainError((Y, Z₁, R), "Y and Z₁ must be positive and finite, R must be positive"))
-    elseif Z₁ ≥ 1
-        return emptyinterval(NewType)
+function interval_of_existence(Y::Real, Z₁::Real, R::Real; verbose::Bool=false)
+    r = interval(Y)/(interval(one(Z₁)) - interval(Z₁))
+    r_sup = sup(r)
+
+    if R < 0 || isnan(R)
+        verbose && @info "failure: the error threshold R is invalid\nY  = $Y\nZ₁ = $Z₁\nR  = $R\nroot = $r"
+        return emptyinterval(r)
     else
-        r = Y/(one(Interval{NewType}) - Z₁)
-        r_sup = NewType(sup(r), RoundUp)
         if 0 ≤ r_sup ≤ R
-            return interval(NewType, r_sup, R)
+            verbose && @info "success: the root and R delimit an interval of existence\nY  = $Y\nZ₁ = $Z₁\nR  = $R\nroot = $r"
+            ie = IntervalArithmetic._unsafe_bareinterval(IntervalArithmetic.numtype(r), r_sup, R)
+            return IntervalArithmetic._unsafe_interval(ie, min(decoration(r), decoration(ie)), isguaranteed(r))
         else
-            return emptyinterval(NewType)
+            verbose && @info "failure: the root is not in [0, R]\nY  = $Y\nZ₁ = $Z₁\nR  = $R\nroot = $r"
+            return emptyinterval(r)
         end
     end
 end
 
-interval_of_existence(Y::Real, Z₁::Real, R::Interval) = interval_of_existence(Y, Z₁, sup(R))
+interval_of_existence(::Real, ::Real, R::Interval; verbose::Bool=false) = throw(ArgumentError("R cannot be an interval, got $R"))
 
 """
     interval_of_existence(Y::Real, Z₁::Real, Z₂::Real, R::Real)
 
 Return an interval of existence ``I \\subset [0, R]`` such that ``Y + (Z_1 - 1) r + Z_2 r^2 / 2 \\le 0`` and ``Z_1 + Z_2 r < 1`` for all ``r \\in I``.
 """
-function interval_of_existence(Y_::Real, Z₁_::Real, Z₂_::Real, R::Real)
-    Y, Z₁, Z₂ = sup(Y_), sup(Z₁_), sup(Z₂_)
-    NewType = float(promote_type(typeof(Y), typeof(Z₁), typeof(Z₂), typeof(R)))
-    if Z₂ == 0
-        return interval_of_existence(Y, Z₁, R)
-    elseif !(Y ≥ 0 && isfinite(Y) && Z₁ ≥ 0 && isfinite(Z₁) && Z₂ ≥ 0 && isfinite(Z₂) && R ≥ 0)
-        return emptyinterval(NewType) # throw(DomainError((Y, Z₁, Z₂, R), "Y, Z₁ and Z₂ must be positive and finite, R must be positive"))
+function interval_of_existence(Y::Real, Z₁::Real, Z₂::Real, R::Real; verbose::Bool=false)
+    iszero(Z₂) && return interval_of_existence(Y, Z₁, R; verbose = verbose)
+
+    b = interval(Z₁) - interval(one(Z₁))
+    Δ = b*b - _safe_mul(2, interval(Z₂))*interval(Y)
+
+    sqrtΔ = sqrt(Δ)
+
+    r₁ = (-b - sqrtΔ)/Z₂
+    r₁_sup = sup(r₁)
+
+    r₂ = (-b + sqrtΔ)/Z₂
+    r₂_inf = inf(r₂)
+
+    if inf(Δ) < 0
+        verbose && @info "failure: both roots are complex\nY  = $Y\nZ₁ = $Z₁\nZ₂ = $Z₂\nR  = $R\ndiscrimant: (Z₁ - 1)² - 2Z₂Y = $Δ"
+        return emptyinterval(r₁)
+    elseif r₁_sup > r₂_inf
+        verbose && @info "failure: could not compute the roots sufficently accuratly\nY  = $Y\nZ₁ = $Z₁\nZ₂ = $Z₂\nR  = $R\ndiscrimant: (Z₁ - 1)² - 2Z₂Y = $Δ"
+        return emptyinterval(r₁)
     else
-        b = Z₁ - one(Interval{NewType})
-        Δ = b*b - 2*interval(Z₂)*Y
-        if inf(Δ) < 0 # complex roots
-            return emptyinterval(NewType)
-        else # real roots
-            sqrtΔ = sqrt(Δ)
-            r₁ = -(sqrtΔ + b)/Z₂
-            r₁_sup = NewType(sup(r₁), RoundUp)
-            if 0 ≤ r₁_sup ≤ R && sup(Z₁ + interval(Z₂) * r₁_sup) < 1
-                r₂ = (sqrtΔ - b)/Z₂
-                r₂_inf = NewType(inf(r₂), RoundDown)
-                if r₁_sup > r₂_inf
-                    return emptyinterval(NewType)
-                elseif 0 ≤ r₂_inf ≤ R && sup(Z₁ + interval(Z₂) * r₂_inf) < 1
-                    return interval(NewType, r₁_sup, r₂_inf)
-                elseif sup(Z₁ + interval(Z₂) * R) < 1
-                    return interval(NewType, r₁_sup, R)
+        if R < 0 | isnan(R)
+            verbose && @info "failure: the threshold R is invalid\nY  = $Y\nZ₁ = $Z₁\nZ₂ = $Z₂\nR  = $R\ndiscrimant: (Z₁ - 1)² - 2Z₂Y = $Δ\nroots = ($r₁, $r₂)"
+            return emptyinterval(r₁)
+        else
+            if 0 ≤ r₁_sup ≤ R && sup(interval(Z₁) + interval(Z₂) * r₁_sup) < 1
+                if 0 ≤ r₂_inf ≤ R && sup(interval(Z₁) + interval(Z₂) * r₂_inf) < 1
+                    verbose && @info "success: both roots delimit an interval of existence\nY  = $Y\nZ₁ = $Z₁\nZ₂ = $Z₂\nR  = $R\ndiscrimant: (Z₁ - 1)² - 2Z₂Y = $Δ\nroots = ($r₁, $r₂)"
+                    ie = IntervalArithmetic._unsafe_bareinterval(IntervalArithmetic.numtype(r₁), r₁_sup, r₂_inf)
+                elseif sup(interval(Z₁) + interval(Z₂) * R) < 1
+                    verbose && @info "success: the smallest root and R delimit an interval of existence\nY  = $Y\nZ₁ = $Z₁\nZ₂ = $Z₂\nR  = $R\ndiscrimant: (Z₁ - 1)² - 2Z₂Y = $Δ\nroots = ($r₁, $r₂)"
+                    ie = IntervalArithmetic._unsafe_bareinterval(IntervalArithmetic.numtype(r₁), r₁_sup, R)
                 else
-                    x = NewType(prevfloat(inf(-b/Z₂)), RoundDown)
-                    if r₁_sup ≤ x ≤ r₂_inf && x ≤ R
-                        return interval(NewType, r₁_sup, x)
+                    z = inf(-b/interval(Z₂))
+                    x = float(z)
+                    while x > z
+                        x = prevfloat(x)
+                    end
+                    if r₁_sup ≤ x ≤ min(r₂_inf, R)
+                        verbose && @info "success: the smallest root and a slightly smaller float than (1-Z₁)/Z₂ delimit an interval of existence\nY  = $Y\nZ₁ = $Z₁\nZ₂ = $Z₂\nR  = $R\ndiscrimant: (Z₁ - 1)² - 2Z₂Y = $Δ\nroots = ($r₁, $r₂)"
+                        ie = IntervalArithmetic._unsafe_bareinterval(IntervalArithmetic.numtype(r₁), r₁_sup, x)
                     else
-                        return interval(NewType, r₁_sup)
+                        verbose && @info "success: the smallest root provides a (singleton) interval of existence\nY  = $Y\nZ₁ = $Z₁\nZ₂ = $Z₂\nR  = $R\ndiscrimant: (Z₁ - 1)² - 2Z₂Y = $Δ\nroots = ($r₁, $r₂)"
+                        ie = IntervalArithmetic._unsafe_bareinterval(IntervalArithmetic.numtype(r₁), r₁_sup, r₁_sup)
                     end
                 end
+                return IntervalArithmetic._unsafe_interval(ie, min(decoration(r₁), decoration(ie)), isguaranteed(r₁))
             else
-                return emptyinterval(NewType)
+                verbose && @info "failure: both roots are either not contained in [0, R], and/or too large\nY  = $Y\nZ₁ = $Z₁\nZ₂ = $Z₂\nR  = $R\n(Z₁ - 1)² - 2Z₂Y = $Δ\nroots = ($r₁, $r₂)"
+                return emptyinterval(r₁)
             end
         end
     end
 end
 
-interval_of_existence(Y::Real, Z₁::Real, Z₂::Real, R::Interval) = interval_of_existence(Y, Z₁, Z₂, sup(R))
+interval_of_existence(::Real, ::Real, ::Real, R::Interval; verbose::Bool=false) = throw(ArgumentError("R cannot be an interval, got $R"))

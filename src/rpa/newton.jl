a867a8959bfa05a2706f1b3fb927a5d80f5b517e
OlivierHnt <38465572+OlivierHnt@users.noreply.github.com>
2024-02-21 14:57:05 +0100

    Clean up newton

--- src/rpa/newton.jl
+++ src/rpa/newton.jl
@@ -1,166 +1,97 @@
-function newton(F_DF, x₀; tol::Real=1e-12, maxiter::Int=15, verbose::Bool=true)
-    tol < 0 || maxiter < 0 && return throw(DomainError((tol, maxiter), "tolerance and maximum number of iterations must be positive"))
-    verbose && return _newton_verbose(F_DF, x₀, tol, maxiter)
-    return _newton_silent(F_DF, x₀, tol, maxiter)
-end
-
-function _newton_silent(F_DF, x₀::Number, tol, maxiter)
+function newton(F_DF::Function, x₀::Number; tol::Real=1e-12, maxiter::Int=15, verbose::Bool=true)
+    (tol < 0) | (maxiter < 0) && return throw(DomainError((tol, maxiter), "tolerance and maximum number of iterations must be positive"))
+    verbose && _display_newton_info(tol, maxiter)
     F, DF = F_DF(x₀)
     nF = abs(F)
-    nF ≤ tol && return x₀, true
-    x = x₀
-    i = 1
-    while i ≤ maxiter && isfinite(nF)
-        x -= DF \ F
-        F, DF = F_DF(x)
-        nF = abs(F)
-        nF ≤ tol && return x, true
-        i += 1
-    end
-    return x, false
-end
-
-function _newton_verbose(F_DF, x₀::Number, tol, maxiter)
-    _display_newton_info(tol, maxiter)
-    F, DF = F_DF(x₀)
-    nF = abs(F)
-    _display_newton_nF(0, nF)
+    verbose && _display_newton_nF(0, nF)
     if nF ≤ tol
-        println()
+        verbose && println()
         return x₀, true
     end
     x = x₀
     i = 1
     while i ≤ maxiter && isfinite(nF)
         AF = DF \ F
-        nAF = abs(AF)
-        _display_newton_nAF(nAF)
+        verbose && _display_newton_nAF(abs(AF))
         x -= AF
         F, DF = F_DF(x)
         nF = abs(F)
-        _display_newton_nF(i, nF)
+        verbose && _display_newton_nF(i, nF)
         if nF ≤ tol
-            println()
+            verbose && println()
             return x, true
         end
         i += 1
     end
-    println()
+    verbose && println()
     return x, false
 end
 
-function _newton_silent(F_DF, x₀, tol, maxiter)
+function newton(F_DF::Function, x₀::Union{AbstractVector,Sequence}; tol::Real=1e-12, maxiter::Int=15, verbose::Bool=true)
+    (tol < 0) | (maxiter < 0) && return throw(DomainError((tol, maxiter), "tolerance and maximum number of iterations must be positive"))
+    verbose && _display_newton_info(tol, maxiter)
     F, DF = F_DF(x₀)
     nF = norm(F, Inf)
-    nF ≤ tol && return x₀, true
-    x = copy(x₀)
-    i = 1
-    while i ≤ maxiter && isfinite(nF)
-        x .-= DF \ F
-        F, DF = F_DF(x)
-        nF = norm(F, Inf)
-        nF ≤ tol && return x, true
-        i += 1
-    end
-    return x, false
-end
-
-function _newton_verbose(F_DF, x₀, tol, maxiter)
-    _display_newton_info(tol, maxiter)
-    F, DF = F_DF(x₀)
-    nF = norm(F, Inf)
-    _display_newton_nF(0, nF)
+    verbose && _display_newton_nF(0, nF)
     if nF ≤ tol
-        println()
+        verbose && println()
         return x₀, true
     end
     x = copy(x₀)
     i = 1
     while i ≤ maxiter && isfinite(nF)
         AF = DF \ F
-        nAF = norm(AF, Inf)
-        _display_newton_nAF(nAF)
+        verbose && _display_newton_nAF(norm(AF, Inf))
         x .-= AF
         F, DF = F_DF(x)
         nF = norm(F, Inf)
-        _display_newton_nF(i, nF)
+        verbose && _display_newton_nF(i, nF)
         if nF ≤ tol
-            println()
+            verbose && println()
             return x, true
         end
         i += 1
     end
-    println()
+    verbose && println()
     return x, false
 end
 
 #
 
-function newton!(F_DF!, x₀, F, DF; tol::Real=1e-12, maxiter::Int=15, verbose::Bool=true)
-    tol < 0 || maxiter < 0 && return throw(DomainError((tol, maxiter), "tolerance and maximum number of iterations must be positive"))
-    verbose && return _newton_verbose!(F_DF!, x₀, F, DF, tol, maxiter)
-    return _newton_silent!(F_DF!, x₀, F, DF, tol, maxiter)
+function newton!(F_DF!::Function, x₀::AbstractVector, F::AbstractVector=similar(x₀), DF::AbstractMatrix=similar(x₀, length(x₀), length(x₀)); tol::Real=1e-12, maxiter::Int=15, verbose::Bool=true)
+    (tol < 0) | (maxiter < 0) && return throw(DomainError((tol, maxiter), "tolerance and maximum number of iterations must be positive"))
+    return _newton!(F_DF!, x₀, F, DF, tol, maxiter, verbose)
 end
 
-function newton!(F_DF!, x₀; tol::Real=1e-12, maxiter::Int=15, verbose::Bool=true)
-    tol < 0 || maxiter < 0 && return throw(DomainError((tol, maxiter), "tolerance and maximum number of iterations must be positive"))
-    F = similar(x₀)
-    n = length(x₀)
-    DF = similar(x₀, n, n)
-    verbose && return _newton_verbose!(F_DF!, x₀, F, DF, tol, maxiter)
-    return _newton_silent!(F_DF!, x₀, F, DF, tol, maxiter)
+function newton!(F_DF!::Function, x₀::Sequence, F::Sequence=similar(x₀), DF::LinearOperator=LinearOperator(space(x₀), space(x₀), similar(coefficients(x₀), length(x₀), length(x₀))); tol::Real=1e-12, maxiter::Int=15, verbose::Bool=true)
+    (tol < 0) | (maxiter < 0) && return throw(DomainError((tol, maxiter), "tolerance and maximum number of iterations must be positive"))
+    return _newton!(F_DF!, x₀, F, DF, tol, maxiter, verbose)
 end
 
-function newton!(F_DF!, x₀::Sequence; tol::Real=1e-12, maxiter::Int=15, verbose::Bool=true)
-    tol < 0 || maxiter < 0 && return throw(DomainError((tol, maxiter), "tolerance and maximum number of iterations must be positive"))
-    F = similar(x₀)
-    s = space(x₀)
-    n = length(x₀)
-    DF = LinearOperator(s, s, similar(coefficients(x₀), n, n))
-    verbose && return _newton_verbose!(F_DF!, x₀, F, DF, tol, maxiter)
-    return _newton_silent!(F_DF!, x₀, F, DF, tol, maxiter)
-end
-
-function _newton_silent!(F_DF!, x₀, F, DF, tol, maxiter)
+function _newton!(F_DF!, x₀, F, DF, tol, maxiter, verbose)
+    verbose && _display_newton_info(tol, maxiter)
     F_DF!(F, DF, x₀)
     nF = norm(F, Inf)
-    nF ≤ tol && return x₀, true
-    i = 1
-    while i ≤ maxiter && isfinite(nF)
-        x₀ .-= DF \ F
-        F_DF!(F, DF, x₀)
-        nF = norm(F, Inf)
-        nF ≤ tol && return x₀, true
-        i += 1
-    end
-    return x₀, false
-end
-
-function _newton_verbose!(F_DF!, x₀, F, DF, tol, maxiter)
-    _display_newton_info(tol, maxiter)
-    F_DF!(F, DF, x₀)
-    nF = norm(F, Inf)
-    _display_newton_nF(0, nF)
+    verbose && _display_newton_nF(0, nF)
     if nF ≤ tol
-        println()
+        verbose && println()
         return x₀, true
     end
     i = 1
     while i ≤ maxiter && isfinite(nF)
         AF = DF \ F
-        nAF = norm(AF, Inf)
-        _display_newton_nAF(nAF)
+        verbose && _display_newton_nAF(norm(AF, Inf))
         x₀ .-= AF
         F_DF!(F, DF, x₀)
         nF = norm(F, Inf)
-        _display_newton_nF(i, nF)
+        verbose && _display_newton_nF(i, nF)
         if nF ≤ tol
-            println()
+            verbose && println()
             return x₀, true
         end
         i += 1
     end
-    println()
+    verbose && println()
     return x₀, false
 end
 

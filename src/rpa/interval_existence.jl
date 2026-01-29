f45e5880ac54244baa4bd07d0a758c3b08955ae6
Olivier Hénot <38465572+OlivierHnt@users.noreply.github.com>
2026-01-30 01:14:09 +0800

    Fix typo

--- src/rpa/interval_existence.jl
+++ src/rpa/interval_existence.jl
@@ -1,11 +1,11 @@
 """
-    interval_of_existence(Y::Interval, Z₁::Interval, R::Real; verbose::Bool = false)
+    interval_of_existence(Y::Interval, Z₁::Interval, R::Real; verbose::Bool=false)
 
 Return an interval and a boolean value with the following meaning:
     - `true`: the interval corresponds to ``I \\subset [0, R]`` such that ``Y + (Z_1 - 1) r \\le 0`` for all ``r \\in I`` and ``Z_1 < 1``.
     - `false`: otherwise, and the interval is empty.
 """
-function interval_of_existence(Y::Interval, Z₁::Interval, R::Real; verbose::Bool = false)
+function interval_of_existence(Y::Interval, Z₁::Interval, R::Real; verbose::Bool=false)
     r = Y/(one(Z₁) - Z₁)
 
     isvalid, msg = _check_inputs(Y, Z₁, R)
@@ -20,7 +20,7 @@ function interval_of_existence(Y::Interval, Z₁::Interval, R::Real; verbose::Bo
     return _setguarantee(interval(IntervalArithmetic.numtype(r), r_sup, R, min(decoration(Y), decoration(Z₁))), t), true
 end
 
-interval_of_existence(Y, Z₁, R; verbose::Bool = false) = interval_of_existence(interval(Y), interval(Z₁), R; verbose = verbose)
+interval_of_existence(Y, Z₁, R; verbose::Bool=false) = interval_of_existence(interval(Y), interval(Z₁), R; verbose = verbose)
 
 function _check_inputs(Y::Interval, Z₁::Interval, R::Real)
     (R < 0) | isnan(R) && return false, "invalid threshold R"
@@ -35,13 +35,13 @@ function _rpa_failure(msg::AbstractString, r::Interval, verbose::Bool)
 end
 
 """
-    interval_of_existence(Y::Interval, Z₁::Interval, Z₂::Interval, R::Real; verbose::Bool = false)
+    interval_of_existence(Y::Interval, Z₁::Interval, Z₂::Interval, R::Real; verbose::Bool=false)
 
 Return an interval and a boolean value with the following meaning:
     - `true`: the interval corresponds to ``I \\subset [0, R]`` such that ``Y + (Z_1 - 1) r + Z_2 r^2 / 2 \\le 0`` and ``Z_1 + Z_2 r < 1`` for all ``r \\in I``.
     - `false`: otherwise, and the interval is empty.
 """
-function interval_of_existence(Y::Interval, Z₁::Interval, Z₂::Interval, R::Real; verbose::Bool = false)
+function interval_of_existence(Y::Interval, Z₁::Interval, Z₂::Interval, R::Real; verbose::Bool=false)
     isthinzero(Z₂) && return interval_of_existence(Y, Z₁, R; verbose = verbose)
 
     b = Z₁ - one(Z₁)
@@ -55,7 +55,7 @@ function interval_of_existence(Y::Interval, Z₁::Interval, Z₂::Interval, R::R
 
     r₁_sup = sup(r₁)
 
-    inf(Δ) < 0                                && return _rpa_failure("discriminant negative → complex roots", r₁, verbose)
+    inf(Δ) < 0 && return _rpa_failure("discriminant negative → complex roots", r₁, verbose)
     0 ≤ r₁_sup ≤ R && sup(Z₁ + Z₂*r₁_sup) < 1 || return _rpa_failure("roots not in [0, R] or contraction fails", r₁, verbose)
 
     verbose && @info "success: interval found\nY = $Y\nZ₁ = $Z₁\nZ₂ = $Z₂\nR = $R\nΔ = $Δ\nroots = ($r₁, $r₂)"
@@ -69,7 +69,7 @@ function interval_of_existence(Y::Interval, Z₁::Interval, Z₂::Interval, R::R
     return _setguarantee(interval(IntervalArithmetic.numtype(r₁), r₁_sup, min(R, x), min(decoration(Y), decoration(Z₁), decoration(Z₂))), t), true
 end
 
-interval_of_existence(Y, Z₁, Z₂, R; verbose::Bool = false) = interval_of_existence(interval(Y), interval(Z₁), interval(Z₂), R; verbose = verbose)
+interval_of_existence(Y, Z₁, Z₂, R; verbose::Bool=false) = interval_of_existence(interval(Y), interval(Z₁), interval(Z₂), R; verbose = verbose)
 
 function _check_inputs(Y::Interval, Z₁::Interval, Z₂::Interval, R::Real)
     (R < 0) | isnan(R) && return false, "invalid threshold R"
@@ -96,19 +96,19 @@ function set_of_radii(Y::AbstractVector{<:Interval}, Z::AbstractMatrix{<:Interva
     Yf = sup.(Y)
     Yf = max.(Yf, floatmin(Float64)) # avoid zeros
 
-    Z₁f = sup.(Z₁)
-    Z₂f = sup.(Z₂)
+    Zf = sup.(Z)
+    Wf = sup.(W)
 
-    WWf = r -> reshape(reshape(Z₂f, :, M) * r, M, M)
-    WW2f = r -> reshape(reshape(permutedims(Z₂f, (1, 3, 2)), :, M) * r, M, M)
+    WWf = r -> reshape(reshape(Wf, :, M) * r, M, M)
+    WW2f = r -> reshape(reshape(permutedims(Wf, (1, 3, 2)), :, M) * r, M, M)
 
-    Pf = r -> Yf .+ Z₁f * r .+ (WWf(r) * r) ./ 2 .- r
-    DPf = r -> Z₁f .+ (WWf(r) .+ WW2f(r)) ./ 2 .- I_mat
+    Pf = r -> Yf .+ Zf * r .+ (WWf(r) * r) ./ 2 .- r
+    DPf = r -> Zf .+ (WWf(r) .+ WW2f(r)) ./ 2 .- I_mat
 
-    WW = r -> reshape(reshape(Z₂, :, M) * r, M, M)
-    Pintval = r -> Y .+ Z₁ * r .+ (WW(r) * r) ./ 2 .- r
+    WW = r -> reshape(reshape(W, :, M) * r, M, M)
+    Pintval = r -> Y .+ Z * r .+ (WW(r) * r) ./ 2 .- r
 
-    Mat = r -> Z₁ .+ WW(r)
+    Mat = r -> Z .+ WW(r)
 
     # Newton iteration to find approximate zero
     r0 = Yf
@@ -197,7 +197,7 @@ function _collatz_wielandt(A)
     return PFupperbound, testvector
 end
 
-set_of_radii(Y, Z, W, R; verbose::Bool = false) = set_of_radii(interval.(Y), interval.(Z₁), interval.(Z₂), R; verbose = verbose)
+set_of_radii(Y, Z, W, R; verbose::Bool=false) = set_of_radii(interval.(Y), interval.(Z), interval.(W), R; verbose = verbose)
 
 function _rpa_failure(msg::AbstractString, verbose::Bool)
     verbose && @info "failure: $msg"
